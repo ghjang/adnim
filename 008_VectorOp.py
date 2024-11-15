7e8fa3238d310462c1fe728795b816e18c216fca
ghjang <jmxexpert@hotmail.com>
2024-11-15 15:23:35 +0900

    공간벡터합 애니메이션 추가

--- 008_VectorOp.py
+++ 008_VectorOp.py
@@ -277,3 +277,143 @@ class VectorDiff(Scene):
         self.play(Write(v_diff_label))
 
         self.wait(2)
+
+
+class Vector3DSum(ThreeDScene):
+    def construct(self):
+        self.next_section("Sum of Two Spatial Vectors Intro")
+
+        addition_title = Text("Sum of Two Spatial Vectors",
+                              font="Montserrat").scale(1.5)
+        addition_title.set_color_by_gradient(
+            RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE)
+        addition_title.to_edge(UP)
+        self.add(addition_title)
+
+        plus_text = Text("+", font="Montserrat").scale(2).shift(DOWN / 2)
+        v1 = Vector([2, 3, 1], color=RED).next_to(
+            plus_text, LEFT, buff=1.5).set_z_index(9000)
+        v2 = Vector([3, -1, 2], color=BLUE).next_to(
+            plus_text, RIGHT, buff=1.5).set_z_index(9000)
+        self.add(plus_text, v1, v2)
+
+        self.wait(1)
+
+        self.next_section("Moving the Vectors to 3D Plane")
+
+        axes = ThreeDAxes()
+        origin_dot = Dot(color=WHITE).set_z_index(
+            9999).move_to(axes.c2p(0, 0, 0))
+
+        self.play(
+            FadeOut(addition_title),
+            FadeOut(plus_text),
+            FadeIn(axes),
+            v1.animate.set_z_index(9998),
+            v2.animate.set_z_index(9998),
+            FadeIn(origin_dot),
+            run_time=2
+        )
+
+        self.play(
+            v1.animate.shift(axes.c2p(0, 0, 0) - v1.get_start()),
+            v2.animate.shift(axes.c2p(0, 0, 0) - v2.get_start()),
+            run_time=1
+        )
+
+        self.wait(1)
+
+        self.next_section("Moving the Axes to the Left-Down")
+
+        left_down_point = LEFT * 2 + DOWN
+        self.play(
+            axes.animate.shift(left_down_point),
+            origin_dot.animate.shift(left_down_point),
+            v1.animate.shift(left_down_point),
+            v2.animate.shift(left_down_point),
+            run_time=1
+        )
+
+        v1_label = MathTex(r"\vec{v_1}", color=RED).next_to(
+            v1, LEFT).shift(RIGHT + UP)
+        v2_label = MathTex(r"\vec{v_2}", color=BLUE).next_to(v2, DOWN)
+        self.play(
+            Write(v1_label),
+            Write(v2_label)
+        )
+
+        self.next_section("Copying v2 and move it to v1's end point")
+
+        move_vector = v1.get_end() - v2.get_start()
+        v2_copy = v2.copy()
+
+        self.play(
+            v2_copy.animate.shift(move_vector),
+            run_time=1
+        )
+
+        dashed_v2 = DashedVMobject(
+            Vector(
+                v2.get_vector(),
+                tip_length=0,
+                color=WHITE,
+                stroke_opacity=0.5
+            ).shift(v1.get_end())
+        )
+
+        self.play(
+            Transform(v2_copy, dashed_v2),
+            run_time=0.5
+        )
+
+        self.next_section("Add the result addition vector")
+
+        plane_origin = left_down_point
+        v_sum = Vector(
+            v1.get_vector() + v2.get_vector(),
+            color=GREEN
+        )
+
+        v_sum.shift(plane_origin - v_sum.get_start())
+
+        self.play(
+            Create(v_sum),
+            run_time=1.5
+        )
+
+        v_sum_label = MathTex(
+            r"\vec{v_1} + \vec{v_2}",
+            color=GREEN
+        ).next_to(v_sum, RIGHT).shift(LEFT * 2)
+        self.play(Write(v_sum_label))
+
+        self.wait(1)
+
+        self.next_section("Add the plane formed by the two vectors")
+
+        # 두 벡터가 형성하는 평면을 추가
+        plane_vertices = [
+            plane_origin,
+            plane_origin + v1.get_vector(),
+            plane_origin + v1.get_vector() + v2.get_vector(),
+            plane_origin + v2.get_vector()
+        ]
+        plane = Polygon(*plane_vertices, color=YELLOW, fill_opacity=0.25)
+
+        self.play(
+            FadeIn(plane),
+            run_time=1
+        )
+
+        self.wait(1)
+
+        self.play(
+            FadeOut(v1_label),
+            FadeOut(v2_label),
+            FadeOut(v_sum_label)
+        )
+
+        # 카메라 뷰를 틀어 3D 느낌을 더해줌
+        self.move_camera(phi=100 * DEGREES, theta=-50 * DEGREES, run_time=3)
+        self.wait(2)
+ 
