055de6f8ccb10e3f25f2dec5d2d160a30ce71559
ghjang <jmxexpert@hotmail.com>
2024-11-15 14:32:38 +0900

    평면벡터차 애니메이션 추가

--- 008_VectorOp.py
+++ 008_VectorOp.py
@@ -134,3 +134,146 @@ class VectorSum(Scene):
         self.play(Write(v_sum_label))
 
         self.wait(2)
+
+
+class VectorDiff(Scene):
+    def construct(self):
+        self.next_section(
+            "Difference of Two Planar Vectors Intro")
+
+        diff_title = Text("Difference of Two Planar Vectors",
+                          font="Montserrat").scale(1.3)
+        diff_title.set_color_by_gradient(
+            RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE)
+        diff_title.to_edge(UP)
+        self.add(diff_title)
+
+        minus_text = Text("-", font="Montserrat").scale(2).shift(DOWN / 2)
+        v1 = Vector([2, 3], color=RED).next_to(
+            minus_text, LEFT, buff=1.5).set_z_index(9000)
+        v2 = Vector([3, 0], color=BLUE).next_to(
+            minus_text, RIGHT, buff=1.5).set_z_index(9000)
+        self.add(minus_text, v1, v2)
+
+        self.wait(1)
+
+        self.next_section("Moving the Vectors to 2D Plane")
+
+        number_plane = NumberPlane(
+            background_line_style={
+                "stroke_opacity": 0.5
+            }
+        )
+        origin_dot = Dot(color=WHITE).set_z_index(
+            9999).move_to(number_plane.coords_to_point(0, 0))
+
+        self.play(
+            FadeOut(diff_title),
+            FadeOut(minus_text),
+            FadeIn(number_plane),
+            v1.animate.set_z_index(9998),
+            v2.animate.set_z_index(9998),
+            FadeIn(origin_dot),
+            run_time=2
+        )
+
+        self.play(
+            v1.animate.shift(number_plane.c2p(0, 0) - v1.get_start()),
+            v2.animate.shift(number_plane.c2p(0, 0) - v2.get_start()),
+            run_time=1
+        )
+
+        self.wait(1)
+
+        self.next_section("Moving the Number Plane to the Down")
+
+        down_point = DOWN
+        self.play(
+            number_plane.animate.shift(down_point),
+            origin_dot.animate.shift(down_point),
+            v1.animate.shift(down_point),
+            v2.animate.shift(down_point),
+            Transform(number_plane, NumberPlane(
+                x_range=[-8, 8],
+                y_range=[-3, 5],
+                background_line_style={
+                    "stroke_opacity": 0.5
+                }
+            )),
+            run_time=1
+        )
+
+        v1_label = MathTex(r"\vec{v_1}", color=RED).next_to(
+            v1, RIGHT).shift(LEFT * 0.7)
+        v2_label = MathTex(r"\vec{v_2}", color=BLUE).next_to(v2, DOWN)
+        self.play(
+            Write(v1_label),
+            Write(v2_label)
+        )
+
+        self.next_section("Add -v2")
+
+        # -v2 벡터 생성
+        v2_neg = Vector(-v2.get_vector(), color=BLUE)
+
+        # 이동된 numberplane의 원점으로 이동 (down_point 사용)
+        v2_neg.shift(down_point)
+
+        # 생성 애니메이션
+        self.play(
+            Create(v2_neg),
+            run_time=1
+        )
+
+        v2_neg_label = MathTex(r"-\vec{v_2}", color=BLUE).next_to(v2_neg, DOWN)
+        self.play(Write(v2_neg_label))
+
+        self.next_section("Move -v2's copy to v1's end point")
+
+        v2_neg_copy = v2_neg.copy()
+
+        # v1의 끝점으로 이동 애니메이션
+        self.play(
+            v2_neg_copy.animate.shift(v1.get_end() - v2_neg.get_start()),
+            run_time=1
+        )
+
+        # 점선 스타일의 벡터 생성
+        dashed_v2_neg = DashedVMobject(
+            Vector(
+                v2_neg.get_vector(),
+                tip_length=0,   # '화살촉' 제거
+                color=WHITE,
+                stroke_opacity=0.5
+            ).shift(v1.get_end())
+        )
+
+        # Transform 실행
+        self.play(
+            Transform(v2_neg_copy, dashed_v2_neg),
+            run_time=0.5
+        )
+
+        self.next_section("Add the result difference vector")
+
+        plane_origin = down_point
+
+        v_diff = Vector(
+            v1.get_vector() - v2.get_vector(),
+            color=GREEN
+        )
+
+        v_diff.shift(plane_origin - v_diff.get_start())
+
+        self.play(
+            Create(v_diff),
+            run_time=1.5
+        )
+
+        v_diff_label = MathTex(
+            r"\vec{v_1} - \vec{v_2}",
+            color=GREEN
+        ).next_to(v_diff, LEFT)
+        self.play(Write(v_diff_label))
+
+        self.wait(2)
