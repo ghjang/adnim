ca1cf7da048bc4cb5455f675dfccad167fa74f32
ghjang <jmxexpert@hotmail.com>
2024-12-03 21:50:12 +0900

    삼각파 플랏 애니메이션 추가, 공통 코드 리팩터링, 일단 동작하는 듯 함.

--- 015_seven_deadly_sines.py
+++ 015_seven_deadly_sines.py
@@ -109,3 +109,81 @@ class SquareWave(CompositeHarmonicScene):
         circle_radius = 1 / (2 * (component_index + 1) - 1)
         angular_velocity = 2 * (component_index + 1) - 1
         return circle_radius, angular_velocity
+
+
+class TriangleWave(CompositeHarmonicScene):
+    """삼각파 플랏 애니메이션"""
+
+    def __init__(self, **kwargs):
+        super().__init__(
+            n_components=7,
+            main_scale=8,
+            transformed_scale=1.2,
+            left_edge_buff=0.25,
+            right_edge_buff=0.25,
+            final_rotation_run_time=8,
+            color_theme='PASTEL',
+            **kwargs
+        )
+
+    @override
+    def construct(self):
+        return super().construct()
+
+    @override
+    def create_title(self):
+        title = Text("Plotting a Triangle Wave", font_size=68)
+        title.set_color_by_gradient(PURPLE_E, GREEN_E, PINK, YELLOW_E)
+        return title
+
+    @override
+    def create_formula_latex(self):
+        elements = []
+        elements.append(r"\sin(x)")
+        for i in range(3, 2 * self.n_components, 2):
+            elements.append("-" if (i-1)//2 % 2 == 1 else "+")
+            elements.append(f"\\frac{{\\sin({i}x)}}{{{i}^2}}")
+
+        color_map = {}
+        term_index = 0
+        for i, elem in enumerate(elements):
+            if (elem not in ["+", "-"]):
+                color_map[elem] = self.colors[f'VECTOR_{term_index + 1}']
+                term_index += 1
+
+        formula_latex = MathTex(*elements, tex_to_color_map=color_map)
+        return formula_latex
+
+    @override
+    def create_plot_plane_config(self):
+        default_config = super().create_plot_plane_config()
+        return {
+            'x_range': [-1, 4 * PI, 1],
+            'y_range': default_config['y_range'],
+            'x_length': 1 + 2 * PI,
+            'y_length': default_config['y_length'],
+        }
+
+    @override
+    def create_graph_plot_function(self):
+        n = self.n_components
+
+        def triangle_wave_func(x):
+            return sum(
+                ((-1)**((k-1)//2)) * np.sin(k * x) / (k*k)
+                for k in range(1, 2 * n, 2)
+            )
+
+        return triangle_wave_func
+
+    @override
+    def create_circle_rotation_info(self, component_index):
+        k = 2 * component_index + 1  # 1, 3, 5, 7, ...
+        circle_radius = 1 / (k * k)  # 1, 1/9, 1/25, 1/49, ...
+
+        # component_index는 '0-base'
+        # '홀수'번째 항의 부호는 '+'
+        # '짝수'번째 항의 부호는 '-'
+        angular_velocity = k if component_index % 2 == 0 else -k
+
+        return circle_radius, angular_velocity
--- _015_seven_deadly_sines_common.py
+++ _015_seven_deadly_sines_common.py
@@ -1,3 +1,4 @@
+from typing import override
 from abc import ABC, abstractmethod
 from manim import *
 
@@ -28,15 +29,15 @@ class CompositeHarmonicScene(Scene, ABC):
     def __init__(
         self,
         n_components=3,
-        main_scale=5,
-        transformed_scale=0.9,
+        main_scale=5,               # 초기 원들 홀로 회전하는 애니메이션에서의 좌표계 크기
+        transformed_scale=0.9,      # 원들과 그래프 플랏 좌표계들 크기
         formula_scale=0.8,
         animation_run_time=8,
-        initial_rotation_time=16,
-        final_rotation_time=32,
+        initial_rotation_run_time=16,
+        final_rotation_run_time=32,
         left_edge_buff=0.5,
         right_edge_buff=0.5,
-        n_final_revolutions=4,
+        bottom_edge_buff=1,
         color_theme='PASTEL',
         **kwargs
     ):
@@ -46,11 +47,11 @@ class CompositeHarmonicScene(Scene, ABC):
         self.transformed_scale = transformed_scale
         self.formula_scale = formula_scale
         self.animation_run_time = animation_run_time
-        self.initial_rotation_time = initial_rotation_time
-        self.final_rotation_time = final_rotation_time
+        self.initial_rotation_time = initial_rotation_run_time
+        self.final_rotation_time = final_rotation_run_time
         self.left_edge_buff = left_edge_buff
         self.right_edge_buff = right_edge_buff
-        self.n_final_revolutions = n_final_revolutions
+        self.bottom_edge_buff = bottom_edge_buff
         self.colors = COLOR_THEMES[color_theme]
 
     @abstractmethod
@@ -68,6 +69,15 @@ class CompositeHarmonicScene(Scene, ABC):
     @abstractmethod
     def create_circle_rotation_info(self, component_index):
         pass
+
+    def create_plot_plane_config(self):
+        """플롯 좌표계 설정을 반환하는 메서드 (오버라이드 가능)"""
+        return {
+            'x_range': [-1, 8 * PI, 1],
+            'y_range': [-2, 2, 1],
+            'x_length': 1 + 2.8 * PI,
+            'y_length': 2 + 2,
+        }
 
     def get_component_config(self, npg, component_index, prev_vector=None):
         """각 컴포넌트의 설정 반환"""
@@ -89,6 +99,7 @@ class CompositeHarmonicScene(Scene, ABC):
             name_suffix=str(component_index)
         )
 
+    @override
     def construct(self):
         """메인 애니메이션 시퀀스"""
 
@@ -96,7 +107,7 @@ class CompositeHarmonicScene(Scene, ABC):
 
         # 타이틀 표시 (있는 경우)
         title = self.create_title()
-        if title:
+        if (title):
             self.play(FadeIn(title))
             self.wait()
             self.play(title.animate.to_edge(UP, buff=0.1).set_opacity(0))
@@ -151,32 +162,39 @@ class CompositeHarmonicScene(Scene, ABC):
         # 수식 추가
         formula = self.create_formula_latex()
         formula.scale(self.formula_scale)
-        formula.to_edge(DOWN, buff=1)
+        formula.to_edge(DOWN, buff=self.bottom_edge_buff)
         self.play(FadeIn(formula))
 
         self.next_section("Setup plot plane", skip_animations=False)
 
         # 플롯 좌표계 생성
-        plot_npg = NumberPlaneGroup(
-            x_range=[-1, 8 * PI, 1],
-            y_range=[-2, 2, 1],
-            x_length=1 + 2.8 * PI,
-            y_length=2 + 2,
-        ).scale(self.transformed_scale).to_edge(RIGHT, buff=self.right_edge_buff).shift(UP)
+        plot_config = self.create_plot_plane_config()
+        plot_npg = NumberPlaneGroup(**plot_config)\
+            .scale(self.transformed_scale)\
+            .to_edge(RIGHT, buff=self.right_edge_buff)\
+            .shift(UP)
 
         self.play(FadeIn(plot_npg))
 
         self.next_section("Final animation", skip_animations=False)
 
+        plot_config = self.create_plot_plane_config()
+
+        # 플롯의 x축 범위에 따라 필요한 회전 수 계산
+        # x축 범위가 2π마다 1회전이므로, 전체 범위를 2π로 나누면 필요한 회전 수가 됨.
+        x_range = plot_config['x_range']
+        total_x_range = x_range[1]                     # '0'부터 시작하므로
+        n_revolutions = int(total_x_range / (2 * PI))  # 2π당 1회전
+
         # 최종 애니메이션
         sine_plot = plot_npg.plot_function(
             self.create_graph_plot_function(),
-            x_range=[0, 8 * PI],
+            x_range=[0, total_x_range],
             color=self.colors['PLOT']
         )
 
         animations = [
-            *manager.create_animations(n_revolutions=self.n_final_revolutions),
+            *manager.create_animations(n_revolutions=n_revolutions),
             manager.create_resultant_animation(),
             CreateWithTracer(
                 sine_plot,
